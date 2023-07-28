d3b9aa1c1daa799eb5bec7c3fade4cb5b118a8b5
Andres Morales <appandres993@gmail.com>
2023-07-28 17:43:55 -0500

    Creamos el componen APP y iniciamos a crear una card tipo twitter

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <article>
+      <header>
+        <img
+          src="https://unavatar.io/duckduckgo/gummibeer.dev"
+          alt="avatar de midudev"
+        />
+        <div>
+          <strong>Andres Felipe M</strong>
+          <span>@Slick993</span>
+        </div>
+      </header>
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
